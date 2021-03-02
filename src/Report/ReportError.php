58e0141bb9ca1d9318b2b845d8afea600f2884cb
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2021-03-02 17:58:15 +0300

    add errors to dictionary

--- src/Report/ReportError.php
+++ src/Report/ReportError.php
@@ -23,18 +23,32 @@ class ReportError
     private $errorMap = [
         'login_login_required' => 'LOGIN_NVALID',
         'login_значение_логин_неверно' => 'LOGIN_NVALID',
+        'login_login_is_invalid' => 'LOGIN_NVALID',
+        'password_пароль_не_соответствует_формату' => 'PASSW_NVALID',
+        'password_password_doesn_t_match_format' => 'PASSW_NVALID',
         'email_value_is_not_unique' => 'EMAIL_NUNIQ',
+        'email_значение_e_mail_не_является_правильным_email_адресом' => 'EMAIL_NVALID',
+        'email_email_required' => 'EMAIL_EMPT',
         'phone_phone_required' => 'PHONE_NVALID',
         'phone_value_is_not_unique' => 'PHONE_NUNIQ',
         'phone_incorrect_phone' => 'PHONE_NVALID',
         'email_e_mail_is_not_a_valid_email_address' => 'EMAIL_NVALID',
         'chief_email_manager_s_e_mail_is_not_a_valid_email_address' => 'CHIEF_EMAIL_NVALID',
         'region_group_is_empty' => 'RGN_EMPT',
+        'groups_group_region_is_required_and_not_blank' => 'RGN_EMPT',
         'city_group_is_empty' => 'CITY_EMPT',
+        'groups_group_city_is_required_and_not_blank' => 'CITY_EMPT',
+        'position_city_is_invalid' => 'CITY_NVALID',
         'role_group_is_empty' => 'ROLE_EMPT',
+        'groups_group_role_is_required_and_not_blank' => 'ROLE_EMPT',
         'position_group_is_empty' => 'PSTN_EMPT',
+        'groups_group_position_is_required_and_not_blank' => 'PSTN_EMPT',
         'team_group_is_empty' => 'TEAM_EMPT',
+        'groups_group_team_is_required_and_not_blank' => 'TEAM_EMPT',
         'assignment_group_is_empty' => 'ASSGN_EMPT',
+        'groups_group_assignment_is_required_and_not_blank' => 'ASSGN_EMPT',
+        'fname_значение_name_должно_содержать_максимум_50_символов' => 'FIRST_NAME_NVALID',
+        'fname_name_should_contain_at_most_50_characters' => 'FIRST_NAME_NVALID',
         'first_name_first_name_required' => 'FIRST_NAME_EMPT',
         'last_name_last_name_required' => 'LAST_NAME_EMPT',
         'first_name_incorrect_data_format_please_try_again' => 'FIRST_NAME_NVALID',
@@ -44,8 +58,8 @@ class ReportError
         'login_login_already_exists' => 'DUBLICAT',
         'phone_phone_number_must_be_min_10_numbers' => 'PHONE_NVALID',
         'email_email_is_not_valid' => 'EMAIL_NVALID',
-        'chief_email_manager_s_e_mail_не_является_правильным_email_адресом' => 'CHIEF_EMAIL_NVALID'
-
+        'chief_email_manager_s_e_mail_не_является_правильным_email_адресом' => 'CHIEF_EMAIL_NVALID',
+        'groups_group_department_is_required_and_not_blank' => 'DEPARTMENT_EMPT',
     ];
 
     /**
